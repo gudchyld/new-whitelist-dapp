ccf4d400d139c016c2a56a7e252e6d9e7afc502a
Elendu Ifeanyi <goodchild66@gmail.com>
2022-07-12 20:22:29 +0100

    First Commit

---
+++ hardhat-tutorial/contracts/whitelist.sol
@@ -0,0 +1,28 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+contract Whitelist {
+
+  uint8 public maxWhitelistedAddresses;
+
+  mapping (address => bool) public whiteListedAddresses;
+
+  uint8 public numAddressesWhitelisted;
+
+  constructor(uint8 _maxWhitelistedAddresses){
+    maxWhitelistedAddresses = _maxWhitelistedAddresses;
+
+  }
+
+  function addAddressToWhitelist() public{
+
+    require(whiteListedAddresses[msg.sender], "Sender has already been whitelisted");
+
+    require(numAddressesWhitelisted < maxWhitelistedAddresses, "more addresses can't be added");
+
+    whiteListedAddresses[msg.sender] = true;
+
+    numAddressesWhitelisted += 1;
+  }
+
+}
